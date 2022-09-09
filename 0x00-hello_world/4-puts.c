fb404874f55eb1e7a895f5b5ba0670e0447bcf7d
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-08 19:23:21 -0700

    Build and compile 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main() {
+int main(void) //This program prints Programming is like building a multilingual puzzle
+{
     char str[] = "\"Programming is like building a multilingual puzzle";
+    
     puts(str);
-    return(0);
+    return (0);
 }
