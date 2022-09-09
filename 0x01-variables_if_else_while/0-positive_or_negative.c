06f76eed1083b68bedd12c10b4b7cebac4721c60
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-09 15:56:27 -0700

    Add condition checker to 0-positive_or_negative.c source code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,17 +11,17 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n>0)
+if (n > 0)
 {
-printf("is positive");
+  printf("%d is positive", n);
 }
-else if (n==0)
+else if (n == 0)
 {
-printf("is zero");
+  printf("%d is zero", n);
 }
 else
 {
-printf("is negative");
+  printf("%d is negative", n);
 }
 return (0);
 }
