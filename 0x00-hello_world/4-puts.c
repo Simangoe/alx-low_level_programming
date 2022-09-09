ee50ab61527e9aa0e083df473e7a3635c5618fe2
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-08 20:56:24 -0700

    Compile 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
  * main - A function to print out a string
  * Return: Return value set to zero [0]
  */
-int main()
+int main(void)
 {
-  char str[] = "\"Programming is like building a multilingual puzzle";
+char str[] = "\"Programming is like building a multilingual puzzle";
 
-  puts(str);
-  return (0);
+puts(str);
+return (0);
 }
