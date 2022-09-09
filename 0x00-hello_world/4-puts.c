8412ecc7d9b108cff9ed7aefad7ad03cd619f66d
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-08 19:53:53 -0700

    Compile 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-/**
- * my_function - This is a description
- */
+/* This is a description */
 int main(void)
 {
 char str[] = "\"Programming is like building a multilingual puzzle";
 
 puts(str);
-/**
-
- * my_function - This is a description
-
- */
+/* This is a description */
 return (0);
 }
