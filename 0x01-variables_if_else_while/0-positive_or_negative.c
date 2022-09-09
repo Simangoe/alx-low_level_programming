9b4dec76e893fd8f811186e2093d5124664337e6
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-09 16:02:17 -0700

    Add condition checker to 0-positive_or_negative.c source code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - enter the function that will decide whether a number is negative or positive
+ * main - enter function to decide number sign
  * Return: return 0
  */
 int main(void)
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-  printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-  printf("%d is zero\n", n);
+printf("%d is zero\n", n);
 }
 else
 {
-  printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
