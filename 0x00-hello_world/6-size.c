bd0c9ec70d85ae6e058f7a41c4d699c760bb6125
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-11 08:30:33 -0700

    Corrections for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,5 @@ printf("Size of an int: %i byte(s)\n", sizeof(b));
 printf("Size of a long int: %ld byte(s)\n", sizeof(c));
 printf("Size of a long long int: %lld byte(s)\n", sizeof(d));
 printf("Size of a float: %d byte(s)\n", sizeof(e));
-return(0);
+return (0);
 }
