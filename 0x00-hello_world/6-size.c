e92e3022d131dea1f9d0cf23094f5375192c1ac7
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-11 08:24:36 -0700

    Corrections for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
  *main-start of function
- *return: 0
+ *Return: 0
  */
 int main (void)
 {
-char a='a';
-int b=10;
-long int c=10;
-long long int d=10;
-float e=10.10;
+char a = 'a';
+int b = 10;
+long int c = 10;
+long long int d = 10;
+float e = 10.10;
 
 printf("Size of a char: %d byte(s)\n", sizeof(a));
 printf("Size of an int: %i byte(s)\n", sizeof(b));
