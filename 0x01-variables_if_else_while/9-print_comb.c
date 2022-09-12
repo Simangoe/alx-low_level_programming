fccaffbf8a2cd338f5e8ef0b9f900622e385653c
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-12 06:33:15 -0700

    Correction to 8-print_base16

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,10 @@ int main(void)
 int i = '0';
 int j = '0';
 for (i = '0'; i <= '9'; i++)
-for (j = '0'; j <= '9';j++)
 {
 putchar(i);
 putchar(44);
-putchar(j);
+putchar(0);
 putchar(32);
 }
 return (0);
