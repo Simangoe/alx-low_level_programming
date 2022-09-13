b642f49866e363a89366ec8d3cc5b7f72d0dfa53
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-13 11:25:47 -0700

    Solve 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry
+ * Return: returns 0 (success)
+ */
+int main(void)
+{
+int i = 97;
+{
+for (int i == 97; i <= 122; i++)
+}
+_putchar('\n');
+}
+return (0);
+}
