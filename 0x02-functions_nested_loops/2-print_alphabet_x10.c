946d2253603119a2eab6f485cedd34d598a961d5
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-13 15:33:30 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print alphabet in lower 10 times
+ * Return: returns 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+int a = 0;
+for (a = 0; a <= 10; a++)
+{
+int i = 'a';
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+}
+_putchar('\n');
+}
