81be882a41d8822bbe0c807989d3688410b1d381
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-13 10:55:42 -0700

    Solve 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include "main.h"
 /**
- *main - entry into program
- *Return: returns 0 (success)
+ * main - entry
+ * Return: returns 0 (success)
  */
-int main(void)
+int main (void)
 {
-printf("_putchar");
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+_putchar('\n');
 return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+int _putchar(int c)
+{
+putchar(c);
+return (0);
+}
