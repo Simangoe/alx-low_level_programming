6135d7820fbae9109a97241ef01bf8b869184156
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-13 17:24:36 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - function to print alphabet in lower 10 times
+ * @c: single letter input
+ * Return: returns 0 (success)
+ */
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c>= 65 && c<= 90))
+return (1);
+else
+return (0);
+}
+
