3a60f2042f3aa60407049333bf445388f92ed19c
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-13 17:13:25 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,13 @@
 #include <ctype.h>
 /**
  * _islower - function to print alphabet in lower 10 times
+ * @c: single letter input
  * Return: returns 0 (success)
  */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
-return(1);
+return (1);
 else
 return (0);
 }
