448b1d415f8806365418daafa2e5dfeaf5c4e31f
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-15 15:13:22 -0700

    Create FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry into program
+ * no Parameter for the function to check
+ * Return: returns 1 when successful
+ */
+
+int main(void)
+{
+int i = 1;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 != 0 && i % 5 != 0)
+printf("%d ", i);
+else
+{
+if ((i % 15) == 0)
+printf("FizzBuzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+}
+}
+printf("\n");
+return (1);
+}
