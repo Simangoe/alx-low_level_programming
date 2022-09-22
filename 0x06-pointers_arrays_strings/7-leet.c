1e934fa9f8e64719055fb0af81b376d1d0570afb
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-22 11:44:10 -0700

    Write a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,31 +8,22 @@
  */
 char *leet(char *x)
 {
+char toBeReplaced[10] = {97, 65, 101, 69, 111, 79, 116, 84, 108, 76};
+char toReplace[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 int i;
-int y = strlen(x);
+int j;
+int strlenX = strlen(x);
+int strlenToBeReplaced = strlen(toBeReplaced);
 
-for (i = 1; i < y; i++)
+for (i = 1; i < strlenX; i++)
 {
-if (isalpha(x[i]) == 97)
+if (x[i] == 97 || x[i] == 65 || x[i] == 101 || x[i] == 69 || x[i] == 111 || x[i] ==79 || x[i] == 116 || x[i] == 84 || x[i] == 108 || x[i] == 76)
 {
-x[i] = 52;
-}
-else if (isalpha(x[i]) == 101)
-{
-x[i] = 51;
-}
-else if (isalpha(x[i]) == 111)
-{
-x[i] = 48;
-}
-else if (isalpha(x[i]) == 116)
-{
-x[i] = 55;
-}
-else if (isalpha(x[i]) == 108)
-{
-x[i] = 49;
+j = 1;
+while (j < strlenToBeReplaced)
+toBeReplaced[j] = toReplace[j];
+j++;
 }
 }
-return x;
+return (x);
 }
