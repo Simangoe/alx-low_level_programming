8a7af5b65ff47be28cac4c832693422bf1d77798
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-22 12:11:36 -0700

    Write a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,21 +2,34 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * cap_string - function to capitalise first letter
- * @x: string to capitalise first letter
- * Return: return character string
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
  */
-char *cap_string(char *x)
+char *cap_string(char *str)
 {
-int i;
-int y = strlen(x);
-
-for (i = 1; i < y; i++)
+int index = 0;
+while (str[index])
 {
-if (isalpha(x[i]) && ((x[i-1] == ' ') || (x[i-1] == '\n') || (x[i-1] == '\t') || (x[i-1] == '.')))
-{
-x[i] = toupper(x[i]);
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
+index == 0)
+str[index] -= 32;
+index++;
 }
-}
-return (x);
+return (str);
 }
