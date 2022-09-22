67726287c43193272047201276b0af21a010e8d6
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-09-22 12:08:53 -0700

    Write a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,26 @@
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
+#include "main.h"
 /**
- * leet - function to encode
- * @x: string to capitalise first letter
- * Return: return character string
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
  */
-char *leet(char *x)
+char *leet(char *str)
 {
-char toBeReplaced[10] = {97, 65, 101, 69, 111, 79, 116, 84, 108, 76};
-char toReplace[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
-int i;
-int j;
-int strlenX = strlen(x);
-int strlenToBeReplaced = strlen(toBeReplaced);
-
-for (i = 1; i < strlenX; i++)
+int indx1 = 0, indx2;
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+while (str[indx1])
 {
-if (x[i] == 97 || x[i] == 65 || x[i] == 101 || x[i] == 69 || x[i] == 111 || x[i] ==79 || x[i] == 116 || x[i] == 84 || x[i] == 108 || x[i] == 76)
+for (indx2 = 0; indx2 <= 7; indx2++)
 {
-j = 1;
-while (j < strlenToBeReplaced)
-toBeReplaced[j] = toReplace[j];
-j++;
+if (str[indx1] == leet[indx2] ||
+str[indx1] - 32 == leet[indx2])
+str[indx1] = indx2 + '0';
 }
+indx1++;
 }
-return (x);
+return (str);
 }
