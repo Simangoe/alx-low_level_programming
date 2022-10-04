e8544bf59723d871f26d0a708322e27211f9e414
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-10-04 04:00:04 -0700

    update function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *concat = (char *)malloc(sizeof(s1 - 1) + sizeof(s2));
+char *concat;
 int lenofs1;
 int lenofs2;
 int lenofs12;
@@ -20,6 +20,9 @@ int i, j = 0;
 (s1 == NULL) ? (s1 = "", lenofs1 = 0) : (lenofs1 = strlen(s1));
 (s2 == NULL) ? (s2 = "", lenofs2 = 0) : (lenofs2 = strlen(s2));
 lenofs12 = lenofs1 + lenofs2 - 1;
+concat = (char *)malloc((lenofs1 + lenofs2) * sizeof(char));
+if (concat != NULL)
+{
 for (i = 0; i < lenofs1; i++)
 {
 concat[i] = s1[i];
@@ -28,13 +31,10 @@ for (i = (lenofs1); i <= lenofs12; i++, j++)
 {
 concat[i] = s2[j];
 }
-if (concat != NULL)
 {
 return (concat);
 }
-else
-{
-return (NULL);
 }
+return (NULL);
 free(concat);
 }
