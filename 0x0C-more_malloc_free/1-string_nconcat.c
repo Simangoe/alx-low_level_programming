ebce3e72aa62c4170843e6706c227d1e40957005
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-10-06 16:22:47 -0700

    function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,44 +10,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *strcn;
-size_t a, c, i, j;
+int count, count1;
+int sign = n;
+char *ptr;
+int len1, len2;
 
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
-}
-if (n < strlen(s2))
+for (len1 = 0; s1[len1] != '\0'; len1++);
+for (len2 = 0; s2[len2] != '\0'; len2++);
+if (sign >= len2)
 {
-a = sizeof(char) * strlen(s1) + n + 1;
-c = strlen(s1) + n + 1;
+sign = len2;
+ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 }
 else
-{
-a = sizeof(char) * (strlen(s1) + strlen(s2) + 1);
-c = strlen(s1) + strlen(s2) + 1;
-}
-strcn = malloc(a);
-if (strcn != NULL)
-{
-for (i = 0; i <= strlen(s1); i++)
-{
-strcn[i] = s1[i];
-}
-for (i = strlen(s1); i < c; i++, j++)
-{
-if (i < c - 1)
-strcn[i] = s2[j];
-else
-strcn[i] = '\0';
-}
-return (strcn);
-free(strcn);
-}
+ptr = malloc(sizeof(char) * (len1 + n + 1));
+if (ptr == NULL)
 return (NULL);
-free(strcn);
+for (count = 0; count < len1; count++)
+{
+ptr[count] = s1[count];
+}
+for (count1 = 0; count1 < sign; count1++)
+{
+ptr[count++] = s2[count1];
+}
+ptr[count++] = '\0';
+return (ptr);
 }
