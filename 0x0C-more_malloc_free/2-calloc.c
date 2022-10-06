acdbd12622d12f4d6c10d240b76adb1070f58be2
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-10-06 16:14:55 -0700

    Write a function that allocates memory for an array, using malloc.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,21 +9,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  char *ptr;
-  unsigned int count;
+char *ptr;
+unsigned int count;
 
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  ptr = malloc(nmemb * size);
-  if (ptr == NULL)
-    {
-      return (NULL);
-    }
-  count = 0;
-  while (count < nmemb * size)
-    {
-      ptr[count] = 0;
-      count++;
-    }
-  return (ptr);
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+{
+return (NULL);
+}
+count = 0;
+while (count < nmemb * size)
+{
+ptr[count] = 0;
+count++;
+}
+return (ptr);
 }
