009fe1d2cd654ff4b24a6b45da6c7a20f936aacb
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-10-12 12:39:54 -0700

    function that prints name string

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_name - print name
+ * @name: name to print
+ * @f: pointer to a function with name input
+ * Return: null
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
