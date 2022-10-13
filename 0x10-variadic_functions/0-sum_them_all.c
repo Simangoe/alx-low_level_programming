5c64eb8afbcae91ae0a28888d38c2b3b9271d25e
Nigel Kanyesi <nigelkanyesi@gmail.com>
2022-10-13 15:10:56 -0700

    make function that computes all arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,23 +3,23 @@
 /**
  * sum_them_all - add all arguments
  * @n: number of arguments
- * Return: sum of arguments, 0 if n == 0
+ * Return: sum of arguments
  */
 int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
 unsigned int i;
-signed int value = 0;
+int value = 0;
 
-if (n == 0)
+if (n != 0)
 {
-return (0);
-}
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
-value = value + va_arg(args, signed int);
+value = value + va_arg(args, int);
 }
 va_end(args);
 return (value);
+}
+return (0);
 }
